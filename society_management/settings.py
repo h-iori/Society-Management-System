d52544ef9202435cdf8547d2c0ef2a04cad0b70f
Harsh Upadhyay <harshupadhyay9702@gmail.com>
2026-02-12 00:30:24 +0530

    static

--- society_management/settings.py
+++ society_management/settings.py
@@ -125,7 +125,7 @@ DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL= 'static/'
+STATIC_URL= '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # Recommended for project-level assets
 ]
